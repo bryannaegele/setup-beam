e7fa331f7b27ae2546c77d272aaabcb777cd73ec
Bryan Naegele <bryannaegele@users.noreply.github.com>
2021-08-02 14:47:10 -0600

    Update install-elixir.sh

--- dist/install-elixir.sh
+++ dist/install-elixir.sh
@@ -12,3 +12,5 @@ DIR_FOR_BIN=.setup-beam/elixir
 wget -q -O "${FILE_OUTPUT}" "https://repo.hex.pm/builds/elixir/${FILE_INPUT}"
 mkdir -p "${DIR_FOR_BIN}"
 unzip -q -d "${DIR_FOR_BIN}" "${FILE_OUTPUT}"
+
+echo ls -al $DIR_FOR_BIN
